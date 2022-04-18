e0dd3306495c64acf7a9281f56396eec18894515
Tristeamer <94586110+Tristeamer@users.noreply.github.com>
2022-04-18 04:02:56 -0600

    Update setupthing.sh

--- setupthing.sh
+++ setupthing.sh
@@ -12,4 +12,26 @@ cd ~/Desktop/setup-thing
 
 chmod u+x setup.sh
 
+cd 
+
+cd ~/Desktop/setup-thing/DebInstaller
+
+chmod u+x Installer.sh
+
+cd 
+
+cd ~/Desktop/setup-thing/PackageManager
+
+chmod u+x PackageManagers.sh
+
+cd 
+
+cd ~/Desktop/setup-thing/Uninstaller
+
+chmod u+x Uninstaller.sh
+
+cd 
+
+cd ~/Desktop/setup-thing/
+
 ./setup.sh
