fad8f7057a59029566e46ce48b0a98bb76e4cefc
Tristeamer <94586110+Tristeamer@users.noreply.github.com>
2022-04-18 02:47:26 -0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt install dialog
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="This script is probably broken as hell by the time you're using it!"
+TITLE="Welcome to Quicker Setup V1.0"
+MENU="What would you like to do?"
+
+OPTIONS=(1 "Install Software"
+         2 "Uninstall Software"
+)
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "Opening PackageManagers"
+            cd
+            cd   ~/Desktop/SetupFaster/PackageManager
+            ./PackageManagers.sh
+            ;;
+        2)
+            echo "Opening Uninstaller"
+            cd
+            cd ~/Desktop/SetupFaster/Uninstaller
+            ./Uninstaller.sh
+            ;;
+esac
